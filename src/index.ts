5914054013bd780dcfdbc96deffc7f584f7a9b9a
Dirag Biswas <hey@diragb.dev>
2024-10-02 20:16:53 +0530

    fix: type inference when `toCSS` is `true`

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ export interface PastellifyOptions {
   toCSS?: boolean
 }
 
+export type PastellifyResult<T extends PastellifyOptions | undefined> = T extends { toCSS: true } ? string : [ number, number, number ]
 
 // Functions:
 const c2n = (c: string): number => Math.abs(isNaN(parseInt(c)) ? c.toLowerCase().charCodeAt(0) - 96 : isNaN(parseInt(c)) ? 0 : parseInt(c))
 const s2n = (s: string): number => s.length > 1 ? parseInt(s.split('').reduce((a, c) => `${ c2n(a) + c2n(c) }`)) : c2n(s)
 const postProcess = (n: [ number, number, number ], options?: PastellifyOptions) => options?.toCSS ? `hsl(${ n[0] }, ${ n[1] }%, ${ n[2] }%)` : n
-const pastellify = (s: string, options?: PastellifyOptions): number[] | string => postProcess([ (3 * s2n(s) + 2 * s2n(s) + s2n(s)) % 360, options?.saturation ?? 78, options?.lightness ?? 73 ], options)
-
+const pastellify = <T extends PastellifyOptions | undefined>(s: string, options?: T): PastellifyResult<T> => postProcess([ (3 * s2n(s) + 2 * s2n(s) + s2n(s)) % 360, options?.saturation ?? 78, options?.lightness ?? 73 ], options) as PastellifyResult<T>
 
 // Exports:
 export default pastellify
